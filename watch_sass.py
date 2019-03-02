f35e2425454e9a5640e44ec12e18db33152ab969
nathantspencer <nathantspencer@gmail.com>
2019-03-02 00:31:07 -0500

    Moved files to be served correctly by flask.

--- watch_sass.py
+++ watch_sass.py
@@ -1,4 +1,4 @@
 import subprocess
 
 print('Watching scss directory for changes...')
-subprocess.check_output(['sass','--watch', 'static/scss/:static/css/'])
+subprocess.check_output(['sass','--watch', 'alloy/static/scss/:alloy/static/css/'])
